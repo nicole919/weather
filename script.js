b7530fc932eacb48db5c09621155c9e5a5340900
Nicole Fafard <nicole.fafard@gmail.com>
2019-09-13 13:33:26 -0700

    updates to owm url

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getWeatherYou() {
     if (city != '') {
 
         $.ajax({
-            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&APPID=e03c2a2f3a20aa1dad04f3d7ed15b3ae',
+            url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&APPID=e03c2a2f3a20aa1dad04f3d7ed15b3ae',
             type: "GET",
             dataType: "jsonp",
             success: function (data) {
@@ -34,7 +34,7 @@ function getWeatherYou() {
 function youResults(data) {
     return '<h2 style="font-weight: bold; font-size:30px; padding-top:30px;" class="text-center">Current Weather for ' + data.name + ', ' + data.sys.country + '</h2>' +
         "<h3 style='padding-left:40px;'><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
-        "<h3 style='padding-left:40px;'><strong>Description</strong>:<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'> " + data.weather[0].description + "</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Description</strong>:<img src='https://openweathermap.org/img/w/" + data.weather[0].icon + ".png'> " + data.weather[0].description + "</h3>" +
         "<h3 style='padding-left:40px;'><strong>Temperature</strong>: " + data.main.temp + " &deg;F</h3>" +
         "<h3 style='padding-left:40px;'><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
         "<h3 style='padding-left:40px;'><strong>Min Temperature</strong>: " + data.main.temp_min + " &deg;F</h3>" +
@@ -55,7 +55,7 @@ function getWeatherThem() {
     if (city != '') {
 
         $.ajax({
-            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&APPID=e03c2a2f3a20aa1dad04f3d7ed15b3ae',
+            url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&APPID=e03c2a2f3a20aa1dad04f3d7ed15b3ae',
             type: "GET",
             dataType: "jsonp",
             success: function (data) {
