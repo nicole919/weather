b89d281d95f592218fa13a82d20a8ab3f441916e
Nicole Fafard <nicole.fafard@gmail.com>
2019-09-13 12:08:11 -0700

    rough draft (MVP)
    minimally (but working) wireframe of weather app

--- script.js
+++ script.js
@@ -1,100 +1,84 @@
-'use strict';
+$(document).ready(function () {
+    $('#getYouWeather').click(function () {
+        return getWeatherYou();
 
-$(() => {
-    function formatQueryParams(params) {
-        const queryItems = Object.keys(params).map(key => `${[encodeURIComponent(key)]}=${encodeURIComponent(params[key])}`);
-        return queryItems.join('&');
-    }
-    // form for "you"
-    function displayResults(responseJson) {
-        console.log(responseJson);
-        
-        $('#js-error').empty();
-        $('#results-you').empty();
-        $('#results-you').removeClass('hidden');
-    }
-    
-    function getWeather(baseUrl, cityName, apiKey) {
-       
-        const params = {
-            q: cityName,
-            appid: 'e03c2a2f3a20aa1dad04f3d7ed15b3ae'
-        }
-       
-        const queryString = formatQueryParams(params);
-        const url = baseUrl + '?' + queryString;
-        console.log(url);
-       
-        
-        fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson => displayResults(responseJson))
-        .catch(err => {
-            $('#js-error').text("Something went wrong");
-        });
-    }
-    
-    
-        $('#you-form').on('submit', function() {
-            event.preventDefault();
-            const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
-            const cityName = $('#js-city-search').val();
-            console.log(cityName)
-    
-            getWeather(baseUrl, cityName);
-        })
-
-        
-}
-
-
-// form for "them"
-function displayResults(responseJson) {
-    console.log(responseJson);
-    
-    $('#js-error').empty();
-    $('#results-them').empty();
-    $('#results-them').removeClass('hidden');
-}
-
-function getWeather(baseUrl, cityName, apiKey) {
-   
-    const params = {
-        q: cityName,
-        appid: 'e03c2a2f3a20aa1dad04f3d7ed15b3ae'
-    }
-   
-    const queryString = formatQueryParams(params);
-    const url = baseUrl + '?' + queryString;
-    console.log(url);
-   
-    
-    fetch(url)
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-        throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-        $('#js-error').text("Something went wrong");
     });
+});
+
+function getWeatherYou() {
+    let city = $('#city').val();
+
+    if (city != '') {
+
+        $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&APPID=e03c2a2f3a20aa1dad04f3d7ed15b3ae',
+            type: "GET",
+            dataType: "jsonp",
+            success: function (data) {
+                let widget = youResults(data)
+
+                $('#results-you').html(widget);
+                $('#city').val('');
+
+            }
+        });
+
+
+    } else {
+        $('#js-error').html('something went wrong');
+    }
 }
 
 
-    $('#them-form').on('submit', function() {
-        event.preventDefault();
-        const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
-        const cityName = $('#js-city-search').val();
-        console.log(cityName)
 
-        getWeather(baseUrl, cityName);
-    })
+function youResults(data) {
+    return '<h2 style="font-weight: bold; font-size:30px; padding-top:30px;" class="text-center">Current Weather for ' + data.name + ', ' + data.sys.country + '</h2>' +
+        "<h3 style='padding-left:40px;'><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Description</strong>:<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'> " + data.weather[0].description + "</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Temperature</strong>: " + data.main.temp + " &deg;F</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Min Temperature</strong>: " + data.main.temp_min + " &deg;F</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Max Temperature</strong>: " + data.main.temp_max + " &deg;F</h3>";
+
+}
+
+//"them" container
+$('#getThemWeather').click(function () {
+    return getWeatherThem();
 
 });
+
+
+function getWeatherThem() {
+    let city = $('#city2').val();
+
+    if (city != '') {
+
+        $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&APPID=e03c2a2f3a20aa1dad04f3d7ed15b3ae',
+            type: "GET",
+            dataType: "jsonp",
+            success: function (data) {
+                let widget = themResults(data)
+                $('#results-them')
+                    .html(widget);
+                $('#city').val('');
+
+            }
+        });
+
+
+    } else {
+        $('#js-error2').html('something went wrong');
+    }
+}
+
+function themResults(data) {
+    return '<h2 style="font-weight: bold; font-size:30px; padding-top:30px;" class="text-center">Current Weather for ' + data.name + ', ' + data.sys.country + '</h2>' +
+        "<h3 style='padding-left:40px;'><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Description</strong>:<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'> " + data.weather[0].description + "</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Temperature</strong>: " + data.main.temp + " &deg;F</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Min Temperature</strong>: " + data.main.temp_min + " &deg;F</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Max Temperature</strong>: " + data.main.temp_max + " &deg;F</h3>";
+}
