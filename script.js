c8575f4d64203889fa7d3e0705b7f06d435081f2
Nicole Fafard <nicole.fafard@gmail.com>
2019-09-16 20:45:57 -0700

    added functionality to temp buttons and some styling

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+let units = 'imperial';
+
 $(document).ready(function () {
     $('#getYouWeather').click(function () {
         return getWeatherYou();
-
-
     });
 
     activatePlacesSearch();
 
+
+    //geocoding
     $('#you-location').click(function (event) {
         event.preventDefault();
         console.log(google.maps);
@@ -16,7 +18,6 @@ $(document).ready(function () {
                 console.log(getCityGoogle(results))
                 const city = getCityGoogle(results);
                 $('#city').val(city);
-                getWeatherYou();
             })
         })
     })
@@ -33,56 +34,32 @@ function getCityGoogle(googleGeocodeResult) {
     return city;
 }
 
-function getWeatherYou() {
-    let city = $('#city').val();
-
-    if (city != '') {
-        $.ajax({
-            url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&APPID=e03c2a2f3a20aa1dad04f3d7ed15b3ae',
-
-            type: "GET",
-            dataType: "jsonp",
-            success: function (data) {
-                let widget = youResults(data)
-
-                $('#results-you').html(widget);
-                $('#city').val('');
-
-            }
-        });
-
-
-    } else {
-        $('#js-error').html('something went wrong');
-    }
-}
-
+//you results
 
 function youResults(data) {
-    return '<h2 style="font-weight: bold; font-size:30px; padding-top:30px;" class="text-center">Current Weather for ' + data.name + ', ' + data.sys.country + '</h2>' +
+    const unitText = units === 'imperial' ? 'F' : 'C';
+    return '<h2 style="font-weight: bold; font-size:50px; padding-top:30px;" class="text-center">Current Weather for ' + data.name + ', ' + data.sys.country + '</h2>' +
         "<h3 style='padding-left:40px;'><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
         "<h3 style='padding-left:40px;'><strong>Description</strong>:<img src='https://openweathermap.org/img/w/" + data.weather[0].icon + ".png'> " + data.weather[0].description + "</h3>" +
-        "<h3 style='padding-left:40px;'><strong>Temperature</strong>: " + data.main.temp + " &deg;F</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Temperature</strong>: " + data.main.temp + " &deg;" + unitText + "</h3>" +
         "<h3 style='padding-left:40px;'><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
-        "<h3 style='padding-left:40px;'><strong>Min Temperature</strong>: " + data.main.temp_min + " &deg;F</h3>" +
-        "<h3 style='padding-left:40px;'><strong>Max Temperature</strong>: " + data.main.temp_max + " &deg;F</h3>";
+        "<h3 style='padding-left:40px;'><strong>Min Temperature</strong>: " + data.main.temp_min + " &deg;" + unitText + "</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Max Temperature</strong>: " + data.main.temp_max + " &deg;" + unitText + "</h3>";
 }
 
-//"them" container
-
-
-
+//them results
 function themResults(data) {
-    return '<h2 style="font-weight: bold; font-size:40px; padding-top:30px;" class="text-center">Current Weather for ' + data.name + ', ' + data.sys.country + '</h2>' +
-        "<h3 style='padding-left:40px; font-size: 50px;'><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
-        "<h3 style='padding-left:40px; font-size: 50px;'><strong>Description</strong>:<img src='https://openweathermap.org/img/w/" + data.weather[0].icon + ".png'> " + data.weather[0].description + "</h3>" +
-        "<h3 style='padding-left:40px; font-size: 50px;'><strong>Temperature</strong>: " + data.main.temp + " &deg;F</h3>" +
-        "<h3 style='padding-left:40px; font-size: 50px;'><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
-        "<h3 style='padding-left:40px; font-size: 50px;'><strong>Min Temperature</strong>: " + data.main.temp_min + " &deg;F</h3>" +
-        "<h3 style='padding-left:40px; font-size: 50px;'><strong>Max Temperature</strong>: " + data.main.temp_max + " &deg;F</h3>";
+    const unitText = units === 'imperial' ? 'F' : 'C';
+    return '<h2 style="font-weight: bold; font-size:50px; padding-top:30px;" class="text-center">Current Weather for ' + data.name + ', ' + data.sys.country + '</h2>' +
+        "<h3 style='padding-left:40px;'><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Description</strong>:<img src='https://openweathermap.org/img/w/" + data.weather[0].icon + ".png'> " + data.weather[0].description + "</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Temperature</strong>: " + data.main.temp + " &deg;" + unitText + "</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Min Temperature</strong>: " + data.main.temp_min + " &deg;" + unitText + "</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Max Temperature</strong>: " + data.main.temp_max + " &deg;" + unitText + "</h3>";
 }
 
-//autofill????
+//autofill functions
 function activatePlacesSearch() {
     let options = {
         types: ['(cities)'],
@@ -117,20 +94,85 @@ function activatePlacesSearch() {
 
         })
     })
+
+    //toggle temp buttons
+
+    $('#metric').click((e) => {
+        e.preventDefault();
+        $('#metric').addClass('selected');
+        $('#imperial').removeClass('selected');
+        units = "metric";
+        const place = autocomplete.getPlace();
+
+        if (place) {
+            const latitude = place.geometry.location.lat();
+            const longitude = place.geometry.location.lng();
+            getWeather(latitude, longitude, (data) => {
+                let widget = youResults(data)
+                $('#results-you').html(widget);
+            })
+        }
+    })
+
+    $('#metric2').click((e) => {
+        e.preventDefault();
+        $('#metric2').addClass('selected');
+        $('#imperial2').removeClass('selected');
+        units = "metric";
+        const place = autocomplete2.getPlace();
+        if (place) {
+            const latitude = place.geometry.location.lat();
+            const longitude = place.geometry.location.lng();
+            getWeather(latitude, longitude, (data) => {
+                let widget = youResults(data)
+                $('#results-them').html(widget);
+            })
+        }
+    })
+
+    $('#imperial').click((e) => {
+        e.preventDefault();
+        $('#imperial').addClass('selected');
+        $('#metric').removeClass('selected');
+        units = "imperial";
+        const place = autocomplete.getPlace();
+        if (place) {
+            const latitude = place.geometry.location.lat();
+            const longitude = place.geometry.location.lng();
+            getWeather(latitude, longitude, (data) => {
+                let widget = youResults(data)
+                $('#results-you').html(widget);
+            })
+        };
+
+    })
+
+    $('#imperial2').click((e) => {
+        e.preventDefault();
+        $('#imperial2').addClass('selected');
+        $('#metric2').removeClass('selected');
+        units = "imperial";
+        const place = autocomplete2.getPlace()
+        if (place) {
+            const latitude = place.geometry.location.lat();
+            const longitude = place.geometry.location.lng();
+            getWeather(latitude, longitude, (data) => {
+                let widget = youResults(data)
+                $('#results-them').html(widget);
+            })
+        }
+    })
 }
 
+// unit toggle call
 function getWeather(lat, lng, callback) {
-
     $.ajax({
-        url: 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lng + '&units=imperial' + '&APPID=e03c2a2f3a20aa1dad04f3d7ed15b3ae',
+        url: 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lng + '&units=' + units + '&APPID=e03c2a2f3a20aa1dad04f3d7ed15b3ae',
         type: "GET",
         dataType: "jsonp",
         success: function (data) {
             callback(data)
-
         }
     });
-
-
-
 }
+
